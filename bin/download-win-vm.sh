7ae296e9bc0a199991c980ddb5f485651e08d206
Luis Fernando Pardo <lfern70@gmail.com>
2023-01-28 19:38:07 +0100

    uncomment download?!

--- bin/download-win-vm.sh
+++ bin/download-win-vm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#wget -O vm-windows.zip https://aka.ms/windev_VM_virtualbox --show-progress --progress=bar:force
+wget -O vm-windows.zip https://aka.ms/windev_VM_virtualbox --show-progress --progress=bar:force
 unzip vm-windows.zip
 rm vm-windows.zip
