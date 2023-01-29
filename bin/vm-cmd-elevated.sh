d870c56fb11197f3639cd8e7ddf95567f93c43bd
Luis Fernando Pardo <lfern70@gmail.com>
2023-01-30 00:28:36 +0100

    .

--- bin/vm-cmd-elevated.sh
+++ bin/vm-cmd-elevated.sh
@@ -8,3 +8,23 @@ sleep 1
 VBoxManage controlvm $VM_DEV_MACHINE keyboardputscancode aa 9d
 sleep 2
 VBoxManage controlvm $VM_DEV_MACHINE keyboardputscancode 0f 8f 0f 8f 39 b9
+sleep 2
+
+VBoxManage controlvm $VM_DEV_MACHINE keyboardputstring "net user administrator /active:yes"
+VBoxManage controlvm $VM_DEV_MACHINE keyboardputscancode 1c 9c
+
+sleep 1
+
+VBoxManage controlvm $VM_DEV_MACHINE keyboardputstring "net user administrator Demo"
+VBoxManage controlvm $VM_DEV_MACHINE keyboardputscancode 1c 9c
+
+sleep 1
+
+VBoxManage controlvm $VM_DEV_MACHINE keyboardputstring "net user User Demo"
+VBoxManage controlvm $VM_DEV_MACHINE keyboardputscancode 1c 9c
+
+sleep 1
+
+VBoxManage controlvm $VM_DEV_MACHINE keyboardputstring "reg HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f"
+VBoxManage controlvm $VM_DEV_MACHINE keyboardputscancode 1c 9c
+
