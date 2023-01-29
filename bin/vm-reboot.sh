04062879666c3bf5c0ea636f94f0e0fc509701c2
Luis Fernando Pardo <lfern70@gmail.com>
2023-01-30 00:36:06 +0100

    .

--- bin/vm-reboot.sh
+++ bin/vm-reboot.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-VBoxManage guestcontrol "$VM_DEV_MACHINE" --username "$VM_DEV_USER" --password "$VM_DEV_PASS" run --exe "cmd.exe" -- "cmd" "/c" "shutdown /r"
+VBoxManage guestcontrol "$VM_DEV_MACHINE" --username "$VM_DEV_USER" --password "$VM_DEV_PASS" run --exe "cmd.exe" -- "cmd" "/c" "shutdown /r /t 0 /f"
