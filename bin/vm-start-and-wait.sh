3987448af3024e407ba7689c91cde1e413194c87
Luis Fernando Pardo <lfern70@gmail.com>
2023-01-31 20:59:52 +0100

    .

--- bin/vm-start-and-wait.sh
+++ bin/vm-start-and-wait.sh
@@ -1,19 +1,16 @@
-#!/bin/sh
-set -e
+#!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-if [ "M$2" == "M" ]; then
-    echo "uso: `basename $0` local-path mount-point"
-    exit -1
-fi
+set -e
 
 mkdir -p ./init-share
-rm ./init-share/logged.txt
-realpath=`realpath ./init-shre/logged.txt`
+rm -f ./init-share/logged.txt
+folder=`realpath ./init-share`
+file="$folder/logged.txt"
 
-VBoxManage sharedfolder add "$VM_DEV_MACHINE" --name "init-share" --hostpath "$realpath" --automount --auto-mount-point "z:\\"
+#VBoxManage sharedfolder add "$VM_DEV_MACHINE" --name "init-share" --hostpath "$folder" --automount --auto-mount-point "z:\\"
 
-"$SCRIPT_DIR/vm-start.sh" > /dev/null
+#VBoxManage startvm "$VM_DEV_MACHINE" --type headless
 
 while [ 1 == 1 ]; do
     echo "Try to execute"
@@ -21,11 +18,12 @@ while [ 1 == 1 ]; do
     VBoxManage controlvm "$VM_DEV_MACHINE" keyboardputscancode E0 5B 13 E0 DB 93
     sleep 5
     # write command
-    VBoxManage controlvm $VM_DEV_MACHINE keyboardputstring "echo logged > z:\\logged.txt"
+    VBoxManage controlvm $VM_DEV_MACHINE keyboardputstring "cmd.exe /c echo logged > z:\\logged.txt"
     sleep 1
     VBoxManage controlvm $VM_DEV_MACHINE keyboardputscancode 1c 9c
     sleep 5
-    if [ -f "$realpath" ]; then
+    echo $file
+    if [ -f "$file" ]; then
         break;
     fi
 
