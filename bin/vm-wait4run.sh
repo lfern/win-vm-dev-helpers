533368c56a8a4b7e30bae29a1eaa01cbd2478fdf
Luis Fernando Pardo <lfern70@gmail.com>
2023-02-01 13:17:56 +0100

    .

--- bin/vm-wait4run.sh
+++ bin/vm-wait4run.sh
@@ -6,8 +6,7 @@ wait_user_ready4commands() {
     local localfile="$2"
     while [ 1 == 1 ]; do
         echo "Check if user ready for commands..."
-        execute_command "cmd.exe /c echo logged > $file"
-        VBoxManage guestcontrol "$VM_DEV_MACHINE" --username "$VM_DEV_USER" --password "$VM_DEV_PASS" run --exe "cmd.exe" -- "cmd.exe" "/c" "echo logged > $file"
+        VBoxManage guestcontrol "$VM_DEV_MACHINE" --username "$VM_DEV_USER" --password "$VM_DEV_PASS" run --exe "cmd.exe" -- "cmd.exe" "/c" "copy /b NUL $file"
         sleep 5
         if [ -f "$localfile" ]; then
             break;
@@ -27,6 +26,6 @@ localfile="$folder/logged.txt"
 drive="z:\\"
 file="${drive}logged.txt"
 
-wait_windows_ready "$file" "$localfile"
+wait_user_ready4commands "$file" "$localfile"
 echo "Ready ..."
 rm -f "$localfile"
