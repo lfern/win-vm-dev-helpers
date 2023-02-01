1796e031104d1fe343ee2bfd967dfadb0fd14995
Luis Fernando Pardo <lfern70@gmail.com>
2023-02-01 23:23:41 +0100

    .

--- bin/vm-create-vm-quiet.sh
+++ bin/vm-create-vm-quiet.sh
@@ -4,7 +4,7 @@ set -e
 
 #"$SCRIPT_DIR/download-win-vm.sh"
 "$SCRIPT_DIR/import-ova.sh" *.ova
-"$SCRIPT_DIR/vm-set-vrde.sh" lfern \"$VM_DEV_VRDEPASS\""
+"$SCRIPT_DIR/vm-set-vrde.sh" lfern "$VM_DEV_VRDEPASS"
 "$SCRIPT_DIR/vm-init.sh"
 "$SCRIPT_DIR/vm-start-headless.sh"
 "$SCRIPT_DIR/vm-wait4run.sh"
