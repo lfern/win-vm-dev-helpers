9f5bdfda70d9d5c31653745d16d1a72e0426c95b
Luis Fernando Pardo <lfern70@gmail.com>
2023-02-01 07:47:36 +0100

    .

---
+++ bin/vm-set-vrde.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ M"$2" == "M" ]; then
+    echo "Uso: `basename $0` user pass"
+    exit -1
+fi
+
+### Prepare for RDP
+VBoxManage setproperty vrdeauthlibrary "VBoxAuthSimple"
+VBoxManage modifyvm $VM_DEV_MACHINE --vrde on
+VBoxManage modifyvm $VM_DEV_MACHINE --vrdeaddress 0.0.0.0
+VBoxManage modifyvm $VM_DEV_MACHINE --vrdeauthtype external
+
+### Set password for user
+result=`VBoxManage internalcommands passwordhash "$2"`
+IFS=' ' read -r -a array <<< "$result"
+VBoxManage setextradata $VM_DEV_MACHINE "VBoxAuthSimple/users/$1" "${array[-1]}"
