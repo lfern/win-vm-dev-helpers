024a2d039432f6f4214477d6850fcc538d2ca048
Luis Fernando Pardo <lfern70@gmail.com>
2023-02-05 14:59:02 +0100

    .

--- bin/vm-create-vm-quiet.sh
+++ bin/vm-create-vm-quiet.sh
@@ -3,16 +3,24 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 set -e
 
 #"$SCRIPT_DIR/download-win-vm.sh"
+echo "Importing ova ..."
 "$SCRIPT_DIR/import-ova.sh" *.ova
+echo "Setting VRDE config"
 "$SCRIPT_DIR/vm-set-vrde.sh" lfern "$VM_DEV_VRDEPASS"
+echo "Init..."
 "$SCRIPT_DIR/vm-init.sh"
+echo "Start headless"
 "$SCRIPT_DIR/vm-start-headless.sh"
 "$SCRIPT_DIR/vm-wait4run.sh"
-
+echo "Installing vs desktop..."
 "$SCRIPT_DIR/vm-install-vs-desktop.sh"
+echo "Installing rust..."
 "$SCRIPT_DIR/vm-install-rust.sh"
+echo "Installing choco..."
 "$SCRIPT_DIR/vm-install-choco.sh"
+echo "Installing git..."
 "$SCRIPT_DIR/vm-install-git.sh"
 "$SCRIPT_DIR/vm-install-gitpath.sh"
-
+echo "Installing vs desktop..."
+"$SCRIPT_DIR/powershell.sh" Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
 
